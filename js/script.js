b878eff3f7558e2c17571823d888440b44ce2947
u10098p7132 <kroon.rj@gmail.com>
2021-04-15 16:38:24 +0200

    INTRO TO APIS challenge 2

--- js/script.js
+++ js/script.js
@@ -6,7 +6,26 @@ const img = document.querySelector('.img');
 const getImage = async function () {
   const res = await fetch('https://picsum.photos/v2/list?limit=100');
   const images = await res.json();
-  console.log(images);
+
+  selectRandomImage(images);
 };
 
-getImage();
+const selectRandomImage = function (images) {
+  let randomIndex = Math.floor(Math.random() * images.length);
+  const randomImage = images[randomIndex];
+  // console.log(randomIndex);
+  // console.log(randomImage);
+  dislayImage(randomImage);
+};
+
+const dislayImage = function (randomImage) {
+  const author = randomImage.author;
+  const imageAddress = randomImage.download_url;
+  authorSpan.textContent = author;
+  img.src = imageAddress;
+  imgDiv.classList.remove('hide');
+};
+
+button.addEventListener('click', function () {
+  getImage();
+});
